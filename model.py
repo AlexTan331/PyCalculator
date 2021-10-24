c99a752df870fbcf7dac14883595f219fefe3de8
Alex Tan <AlexTAN331@outlook.com>
2021-10-23 22:02:22 -0400

    refactor model algorithm

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ class Model:
         self._init_queue()
 
     def _init_queue(self):
-        self.operation_queue = [''] * 9
+        self.operation_queue = [''] * 99
         self.idx = 0
 
     def calculate(self, value):
@@ -16,12 +16,12 @@ class Model:
         if self.idx % 2 == 0:
             if value.isdigit() or (value == '.' and value not in self.operation_queue[self.idx]):
                 self.operation_queue[self.idx] += value
-            elif value == "%" and self.idx == 0:
+            elif value == "%" and self.idx == 0 and self.operation_queue[0]:
                 self.operation_queue[0] = str(eval(f'{self.operation_queue[0]} / 100'))
-            elif value in self.OPERATORS and self.idx + 1 < len(self.operation_queue):
+            elif value in self.OPERATORS and self.operation_queue[self.idx] and self.idx + 1 < len(self.operation_queue):
                 self.idx += 1
                 self.operation_queue[self.idx] = value
-            elif value == '+/-':
+            elif value == '+/-' and self.operation_queue[self.idx]:
                 operand = self.operation_queue[self.idx]
                 self.operation_queue[self.idx] = operand[1:] if operand[0] == '-' else '-' + operand
             elif value == '=':
